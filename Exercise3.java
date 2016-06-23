9a2577630f13f90cb97079c50bd20f8a9af3d993
James Stephen DV Mariano <james.stephen.dv14@gmail.com>
2016-06-23 10:33:55 +0800

    Format result output

--- Exercise3.java
+++ Exercise3.java
@@ -4,43 +4,84 @@ import java.util.concurrent.*;
 import java.util.function.*;
 
 public class Exercise3 {
-
   private static final int STARTING_LINE_DISTANCE = 10;
+  private static final int MAX_THREADS = 100;
+  private static final Scanner INPUT_SCANNER;
   private static final Consumer<String> DEFAULT_OUTPUT = System.out::println;
 
+  static {
+    INPUT_SCANNER = new Scanner(System.in);
+  }
+
   public static void main(String [] args) {
+    int horseCount = promptUserForInt("Enter the number of horses: ");
+    int finishLineDistance = promptUserForInt("Enter the distance to the finish line: ");
 
     HorseRace race = new HorseRace(
       Stream.generate(() -> new Horse(-STARTING_LINE_DISTANCE))
-            .limit(10)  
+            .limit(horseCount)  
             .peek(h -> h.determineHealth(DEFAULT_OUTPUT))
             .filter(h -> h.isHealthy())
             .collect(Collectors.toCollection(PriorityQueue::new))
     );
 
+    int threadCount = Integer.min(race.size(), MAX_THREADS);
+    ExecutorService exec = Executors.newFixedThreadPool(threadCount);
+
+    while (true) {
+      if (!moveFromBarnToStartingLine(race, exec)) {
+        System.out.println("Something bad happened. The race is aborted.");
+        break;
+      }
+
+      SortedSet<HorseTime> results = 
+        moveFromStartingLineToFinishLine(race, exec, finishLineDistance);
+
+      try {
+        displayRaceResults(results);
+      } catch (NullPointerException e) {
+        System.out.println("Something bad happened. The race is aborted.");
+      }
+      
+      break;
+    }
+
+    exec.shutdown();
+  }
+
+  private static boolean moveFromBarnToStartingLine(HorseRace race, ExecutorService exec) {
+    System.out.println("");
+    System.out.println("Healthy horses moving out from barn");
     System.out.println("");
 
-    ExecutorService pool = Executors.newFixedThreadPool(race.size());
-
-    Set<Callable<HorseTime>> startingLineMovers =
+    Set<Callable<HorseTime>> barnToStartMovers =
       race.stream()
-      .map(h -> new NoBoostHorseMover(h, race, 0, DEFAULT_OUTPUT))
-      .collect(Collectors.toSet());
+          .map(h -> new NoBoostHorseMover(h, race, 0, DEFAULT_OUTPUT))
+          .collect(Collectors.toSet());
 
     try {
-      pool.invokeAll(startingLineMovers);
+      exec.invokeAll(barnToStartMovers);
     } catch (InterruptedException e) {
-      e.printStackTrace();
+      return false;
     }
 
     System.out.println("");
-    System.out.println("All horses at starting line");
+    System.out.println("All healthy horses at starting line");
+    System.out.println("");
+
+    return true;
+  }
+
+  private static SortedSet<HorseTime> moveFromStartingLineToFinishLine(HorseRace race, 
+      ExecutorService exec, int finishLineDistance) 
+  {
+    System.out.println("The race begins!");
     System.out.println("");
 
     Set<Callable<HorseTime>> finishLineMovers = 
       race.stream()
-      .map(h -> new LastPlaceBoostHorseMover(h, race, 100, DEFAULT_OUTPUT))
-      .collect(Collectors.toSet());
+          .map(h -> new LastPlaceBoostHorseMover(h, race, finishLineDistance, DEFAULT_OUTPUT))
+          .collect(Collectors.toSet());
 
     Function<Future<HorseTime>, HorseTime> getHorseTime = f -> {
       try {
@@ -51,33 +92,59 @@ public class Exercise3 {
       }
     };
 
-    SortedSet<HorseTime> times = null;
     try {
-      times = pool.invokeAll(finishLineMovers)
-              .stream()
-              .map(getHorseTime)
-              .collect(Collectors.toCollection(TreeSet::new));
+      return exec.invokeAll(finishLineMovers)
+                 .stream()
+                 .map(getHorseTime)
+                 .collect(Collectors.toCollection(TreeSet::new));
     } catch (InterruptedException e) {
-      e.printStackTrace();
+
     }
 
-    System.out.println();
-    System.out.println("Results: ");
+    return null;
+  }
 
-    int place = 1;
-    long bestTime = times.first().getTime();
-    for (HorseTime time : times) {
-      StringBuilder result = new StringBuilder();
-      result.append(place + ". ");
-      result.append("Horse " + time.getId() + " - +");
-      result.append(time.getTime() - bestTime);
-      result.append("ns");
+  private static void displayRaceResults(SortedSet<HorseTime> results) {
+      System.out.println();
+      System.out.println("The race is over!");
+      System.out.println();
+      System.out.println("Race Results: ");
 
-      System.out.println(result);
+      int place = 1;
+      long bestTime = results.first().getTime();
+      int digits = (int)(Math.log10(results.size())) + 1;
+      System.out.println(digits);
 
-      place++;
-    }
+      for (HorseTime time : results) {
+        StringBuilder result = new StringBuilder();
 
-    pool.shutdown();
+        result.append(String.format("%" + digits + "d. ", place));
+        result.append("Horse ");
+        result.append(String.format("%-" + digits + "d ", time.getId()));
+        result.append(String.format("%+10d ns", time.getTime() - bestTime));
+
+        System.out.println(result);
+
+        place++;
+      }
+  }
+
+  private static int promptUserForInt(String promptMsg) {
+    int input = Integer.MIN_VALUE;
+
+    do {
+      try {
+        input = Integer.parseInt(promptUserForLine(promptMsg));
+      } catch (NumberFormatException e) {
+        DEFAULT_OUTPUT.accept("Please enter an integer value.");
+      }
+    } while (input == Integer.MIN_VALUE);
+
+    return input;
+  }
+
+  private static String promptUserForLine(String promptMsg) {
+    System.out.print(promptMsg);
+    return INPUT_SCANNER.nextLine();
   }
 }
